be1d18c725c52b0fd564ef6ac67ba1661a1d8271
AshutoshIWNL <ashutoshmishra9917@gmail.com>
2024-05-05 13:36:45 +0530

    Add attach script for windows

---
+++ attachScript/monarchAgentStart.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem Check if JAVA_HOME is set
+if "%JAVA_HOME%" == "" (
+    echo JAVA_HOME environment variable is not set. Please set it to the Java installation directory.
+    exit /b 1
+)
+
+set /p agentJar="Enter path to the agent JAR file: "
+set /p configFile="Enter path to the agent config file: "
+set /p agentArgs="Enter arguments to pass to the agent: "
+set /p pid="Enter PID of the target JVM (press Enter to use current JVM): "
+
+if "%pid%" == "" (
+    set pid=0
+)
+
+"%JAVA_HOME%\bin\java" -cp .;"%agentJar%;%JAVA_HOME%\lib\tools.jar" com.asm.mja.attach.AgentAttachCLI -agentJar "%agentJar%" -configFile "%configFile%" -args "%agentArgs%" -pid %pid%
